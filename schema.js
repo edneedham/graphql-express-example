0e52861c48b4764ecc92b31562db85094c040f2a
Edward Needham <edwardneedham123@gmail.com>
2020-07-09 15:04:21 +0100

    added base schema and imported into app

---
+++ schema.js
@@ -0,0 +1,11 @@
+const graphql = require('graphql');
+
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql;
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+
+  })
+})
+
+module.exports = schema;
